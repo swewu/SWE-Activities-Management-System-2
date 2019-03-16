5f9fe4500e200dec7d6f0c4742a20b383d7260b0
namfonnnnn <sp.sudaratphiwon@hotmail.com>
2019-03-16 16:39:45 +0700

    delete  footer

--- app/views/manage/layout.blade.php
+++ app/views/manage/layout.blade.php
@@ -222,28 +222,7 @@
             @yield('content')
           </div>
           <!-- / .content -->
-          <footer class="main-footer d-flex p-2 px-3 bg-white border-top">
-            <ul class="nav">
-              <li class="nav-item">
-                <a class="nav-link" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Services</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Products</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Blog</a>
-              </li>
-            </ul>
-            <span class="copyright ml-auto my-auto mr-2">Copyright © 2019
-              <a href="http://www.wu.ac.th" target="_Blank" rel="nofollow">wu.ac.th</a>
-            </span>
-          </footer>
+         
 
         </main>
       </div>

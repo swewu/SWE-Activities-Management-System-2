fd2ee857d8bb2e739e528e392b970327db1e483c
namfonnnnn <sp.sudaratphiwon@hotmail.com>
2019-03-16 16:11:44 +0700

    software engineering

--- app/views/manage/layout.blade.php
+++ app/views/manage/layout.blade.php
@@ -31,7 +31,7 @@
               <a class="navbar-brand w-100 mr-0" href="{{url('manage')}}" style="line-height: 25px;">
                 <div class="d-table m-auto">
                   <img id="main-logo" class="d-inline-block align-top mr-1" style="max-width: 25px;" src="{{asset('assets/shards-dashboard/images/shards-dashboards-logo.svg')}}" alt="Shards Dashboard">
-                  <span class="d-none d-md-inline ml-1">Software Engineer</span>
+                  <span class="d-none d-md-inline ml-1">Software Engineering</span>
                 </div>
               </a>
               <a class="toggle-sidebar d-sm-inline d-md-none d-lg-none">

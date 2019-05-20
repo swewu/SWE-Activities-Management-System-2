2997f466365d382040e22c6557d3b175c47b5531
jompon123 <Forex@172.16.60.205>
2019-05-20 23:21:23 +0700

    commit profile search

--- app/views/paginate-default.blade.php
+++ app/views/paginate-default.blade.php
@@ -1,16 +1,7 @@
 <?php
-<<<<<<< HEAD
 $presenter = new Illuminate\Pagination\BootstrapPresenter($paginator);
 ?>
 
 <ul class="pagination">
         <?php echo $presenter->render(); ?>
 </ul>
-=======
-    $presenter = new Illuminate\Pagination\BootstrapPresenter($paginator);
-?>
-
-    <ul class="pagination">
-            <?php echo $presenter->render(); ?>
-    </ul>
->>>>>>> e53bbeb41f4f31ebf2cc04570888d65e4226c53c
